24600ed9db191d9d267fb1915240718601ed24bd
MateusResende <mateusresende.resende3@gmail.com>
2022-08-09 13:07:09 -0300

    build

---
+++ src/app/produtos/produtos.component.ts
@@ -0,0 +1,37 @@
+import { isFakeMousedownFromScreenReader } from '@angular/cdk/a11y';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { IProduto, } from '../produtos';
+import { ProdutosService } from '../produtos.service';
+
+@Component({
+  selector: 'app-produtos',
+  templateUrl: './produtos.component.html',
+  styleUrls: ['./produtos.component.css']
+})
+export class ProdutosComponent implements OnInit {
+  produtos: IProduto[] | undefined ; 
+
+  constructor( 
+    private produtosService : ProdutosService,
+    private rout: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+  const produtos = this.produtosService.getAll();
+
+    this.rout.queryParamMap.subscribe(params => {
+      const descricao = params.get("descricao")?.toLowerCase();
+
+        if (descricao){
+           this.produtos = produtos.filter(produto => produto.descricao.toLowerCase().includes(descricao));
+           return;
+        }
+         this.produtos = produtos;
+    })
+
+
+  }
+
+}
